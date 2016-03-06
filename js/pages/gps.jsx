cdaed1ea079aaa82cf5a0b0e8aa383f25d1cde3b
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-03-06 14:54:58 +0100

    Add initial GPS subpage

---
+++ js/pages/gps.jsx
@@ -0,0 +1,87 @@
+import $ from 'jquery';
+import React from 'react';
+import {GoogleMapLoader, GoogleMap, Marker, Polyline} from 'react-google-maps';
+import ScriptjsLoader from 'react-google-maps/lib/async/ScriptjsLoader';
+
+const GPS = React.createClass({
+    getInitialState() {
+        return {
+            mapLoaded: false
+        }
+    },
+
+    mapLoaded() {
+        if (!this.state.mapLoaded) {
+            this.setState({mapLoaded: true});
+            let path = this.refs.polyline.getPath();
+            this.refs.polyline.state.polyline.setOptions({
+                icons: [{
+                    icon: {path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW},
+                    offset: '100%'
+                }]
+            });
+
+            this.request = $.get('http://127.0.0.1:8020/gps/', function (result) {
+                // todo: auto refresh
+                for (let point of result) {
+                    path.push(new google.maps.LatLng(point.latitude, point.longitude));
+                }
+            });
+
+            let gsMarker = this.refs.gsMarker;
+            this.request = $.get('http://127.0.0.1:8020/gsinfo/latest/', function (result) {
+                // todo: get gsinfo on app start
+                gsMarker.state.marker.setPosition({lat: result.latitude, lng: result.longitude});
+            });
+        }
+    },
+
+    render() {
+        return (
+            <div>
+                <ScriptjsLoader
+                    hostname={'maps.googleapis.com'}
+                    pathname={'/maps/api/js'}
+                    query={{v: `3`, libraries: 'common,geometry'}}
+                    loadingElement={
+                        <div>Loading...</div>
+                      }
+                    containerElement={
+                        // todo: fix ugly calc() hack
+                        <div style={{height: 'calc(100% + 96px)', margin: '-48px'}}></div>
+                    }
+                    googleMapElement={
+                        <GoogleMap
+                            options={{
+                                streetViewControl: false
+                            }}
+                            defaultZoom={15}
+                            // todo: set gs position as default center
+                            defaultCenter={{lat: 50.062029, lng: 19.936785}}
+                            onIdle={this.mapLoaded}
+                        >
+                            <Polyline
+                                ref='polyline'
+                                options={{
+                                    geodesic: true,
+                                    strokeColor: '#ff0000',
+                                    strokeOpacity: 1.0,
+                                    strokeWeight: 2
+                                }}
+                            />
+                            <Marker
+                                ref='gsMarker'
+                                options={{
+                                    // todo: marker icon
+                                    // icon: 'assets/groundstation.png'
+                                }}
+                            />
+                        </GoogleMap>
+                    }
+                />
+            </div>
+        );
+    }
+});
+
+export default GPS;
--- js/routing.jsx
+++ js/routing.jsx
@@ -4,11 +4,13 @@ import { Router, Route, IndexRoute } from 'react-router'
 import { App, NoMatch } from './app'
 import Telemetry from './pages/telemetry';
 import SHT from './pages/sht';
+import GPS from './pages/gps';
 
 const AppRoutes = (
     <Route path="/" component={App}>
         <IndexRoute component={Telemetry}/>
         <Route path="sht" component={SHT}/>
+        <Route path="gps" component={GPS}/>
         <Route path="*" component={NoMatch}/>
     </Route>
 );
